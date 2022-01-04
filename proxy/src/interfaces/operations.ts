51dfc7ed05fd70b38e350a756e210481f71fd4a9
bentovsky <jpmbento95@gmail.com>
2022-01-04 17:25:47 +0000

    refactor(proxy): update create operation body interface

--- proxy/src/interfaces/operations.ts
+++ proxy/src/interfaces/operations.ts
@@ -4,6 +4,14 @@ export interface CreateOperationBody {
   token: string;
   amount: string;
   type: Operation;
+  name: {
+    title: 'Dr.';
+    firstName: "Ruizao";
+    middleName: 'P.';
+    lastName: "Escobar";
+    fullName: "Rui";
+  };
+  lei: 'AAAA0012345678901299'
 }
 
 interface CreateOperation extends CreateOperationBody{
