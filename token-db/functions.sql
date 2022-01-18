74b5601da5863346684aa19d08af102bcf35c447
Rui Lopes <rui.lopes@altar.io>
2022-01-18 11:15:57 +0000

    wip(db)

--- token-db/functions.sql
+++ token-db/functions.sql
@@ -63,3 +63,9 @@ FROM tokens t,
 WHERE t.user_id=u.id 
 AND t.active = true
 AND u.phoneNumber = "+447401232937";
+
+SELECT DISTINCT u.*,
+                (CASE WHEN (SELECT COUNT(t1.active) FROM tokens t1 where t1.active = true AND t.user_id = t1.user_id ) > 0 then true else false end) 
+FROM tokens t
+INNER JOIN users u ON t.user_id=u.id 
+AND u.phoneNumber = "+447401232937";
