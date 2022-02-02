f84a72fde892ddbbd8a25d3c5f65fc8c779d37f0
Rui Lopes <rui.lopes@altar.io>
2022-02-02 10:03:35 +0000

    wip(sms-gateway)

--- sms-gateway/src/classes/server.ts
+++ sms-gateway/src/classes/server.ts
@@ -85,7 +85,7 @@ class Server {
     );
     this.app.use(bodyParser.urlencoded({ extended: false }));
 
-    const whitelist = ['TwilioProxy.*', 'PostmanRuntime.*', 'axios.*', 'curl.*'];
+    const whitelist = ['TwilioProxy.*', 'PostmanRuntime.*', 'axios.*', 'curl.*', 'http://localhost:4100'];
     const corsOptions: CorsOptions = {
       origin: (origin, callback) => {
         if (process.env.NODE_ENV === 'test' || (origin && whitelist.some((el) => new RegExp(el).test(origin)))) {
